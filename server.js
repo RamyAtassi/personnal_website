d1e8d71722e2a048147cf65ab3346e4bef9605c7
RamyAtassi <ramy.atassi@gmail.com>
2019-07-01 12:30:54 +0200

    change

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ connectDB();
  * Get port from environment and store in Express.
  */
 
-let port = normalizePort(process.env.PORT || "3001");
+let port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 
 /**
